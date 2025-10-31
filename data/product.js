cf9cb551a747b44d48dfc1bc8243869ac7fb560a
Votre Nom <votre.email@example.com>
2025-10-31 20:41:10 +0100

    first commit

---
+++ data/product.js
@@ -0,0 +1,53 @@
+
+export const PRODUCTS = [  
+{  
+    id: 1,  
+    name: "Lait DHT",  
+    category: "Alimentaire",  
+    price: 12.5,  
+    currency: "MAD",  
+    quantity: 20,  
+    expiryDate: "2025-10-20",  
+    tags: ["frais", "lactose"]  
+},  
+{  
+    id: 2,  
+    name: "PC Portable HP",  
+    category: "Électronique",  
+    price: 4500,  
+    currency: "MAD",  
+    quantity: 5,  
+    expiryDate: null,  
+    tags: ["tech", "portable"]  
+},  
+{  
+    id: 3,  
+    name: "Stylo Bic",  
+    category: "Fourniture",  
+    price: 2.5,  
+    currency: "MAD",  
+    quantity: 100,  
+    expiryDate: null,  
+    tags: ["bureau", "écriture"]  
+},  
+{  
+    id: 4,  
+    name: "Yaourt Danone",  
+    category: "Alimentaire",  
+    price: 8.0,  
+    currency: "MAD",  
+    quantity: 15,  
+    expiryDate: "2024-12-15",  
+    tags: ["frais", "laitage"]  
+},  
+{  
+    id: 5,  
+    name: "Cahier 96p",  
+    category: "Fourniture",  
+    price: 6.0,  
+    currency: "MAD",  
+    quantity: 50,  
+    expiryDate: null,  
+    tags: ["bureau", "papeterie"]  
+}  
+];
